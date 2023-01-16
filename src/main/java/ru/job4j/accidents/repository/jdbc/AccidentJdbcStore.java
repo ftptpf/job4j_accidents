73f422a4bc6d6efc2bbeacb319e23db3bb16d32c
ftptpf <inboxru@yandex.ru>
2023-01-16 09:47:09 +0300

    findAll sql string

--- src/main/java/ru/job4j/accidents/repository/jdbc/AccidentJdbcStore.java
+++ src/main/java/ru/job4j/accidents/repository/jdbc/AccidentJdbcStore.java
@@ -3,13 +3,13 @@ package ru.job4j.accidents.repository.jdbc;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapperResultSetExtractor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
-import ru.job4j.accidents.model.Type;
 import ru.job4j.accidents.repository.Store;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Optional;
 
 @Primary
@@ -23,7 +23,7 @@ public class AccidentJdbcStore implements Store<Accident> {
     }
 
     public Collection<Accident> findAll() {
-        return jdbc.query(
+        /*return jdbc.query(
                 """
                     SELECT
                     accidents.id accidents_id,
@@ -31,10 +31,15 @@ public class AccidentJdbcStore implements Store<Accident> {
                     accidents.description accidents_description,
                     accidents.address accidents_address,
                     types.id types_id,
-                    types.name types_name
+                    types.name types_name,
+                    rules.name rules_name
                     FROM accidents
                     INNER JOIN types
-                    ON accidents.type_id = types.id;
+                    ON accidents.type_id = types.id
+                    INNER JOIN accidents_rules
+                    ON accidents.id = accidents_rules.accident_id
+                    INNER JOIN rules
+                    ON accidents_rules.rule_id = rules.id;
                     """,
                 (resultSet, rowNum) -> {
                     Accident accident = new Accident();
@@ -48,7 +53,26 @@ public class AccidentJdbcStore implements Store<Accident> {
                     accident.setType(type);
                     accident.setRules(new HashSet<>());
                     return accident;
-                });
+                });*/
+        String sql = """
+                SELECT
+                accidents.id accidents_id,
+                accidents.name accidents_name,
+                accidents.description accidents_description,
+                accidents.address accidents_address,
+                types.id types_id,
+                types.name types_name,
+                rules.name rules_name
+                FROM accidents
+                INNER JOIN types
+                ON accidents.type_id = types.id
+                INNER JOIN accidents_rules
+                ON accidents.id = accidents_rules.accident_id
+                INNER JOIN rules
+                ON accidents_rules.rule_id = rules.id;
+                """;
+        ResultSetExtractor<Accident> resultSetExtractor = new
+        return jdbc.;
     }
 
     public Optional<Accident> findById(int id) {
