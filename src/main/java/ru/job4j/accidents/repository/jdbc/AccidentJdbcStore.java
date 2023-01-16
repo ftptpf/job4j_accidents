eaf3c2d7f9d58acb8db7719dd66711b7d3cc6f74
ftptpf <Nb49Rtdd>
2023-01-16 22:12:49 +0300

    RowMapper<Accident> and RowMapper<Rule>

--- src/main/java/ru/job4j/accidents/repository/jdbc/AccidentJdbcStore.java
+++ src/main/java/ru/job4j/accidents/repository/jdbc/AccidentJdbcStore.java
@@ -4,13 +4,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.RowMapperResultSetExtractor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.model.Rule;
+import ru.job4j.accidents.model.Type;
 import ru.job4j.accidents.repository.Store;
 
-import java.util.Collection;
-import java.util.Optional;
+import java.util.*;
 
 @Primary
 @Repository
@@ -71,8 +73,7 @@ public class AccidentJdbcStore implements Store<Accident> {
                 INNER JOIN rules
                 ON accidents_rules.rule_id = rules.id;
                 """;
-        ResultSetExtractor<Accident> resultSetExtractor = new
-        return jdbc.;
+        return jdbc.query(sql, accidentRowMapper);
     }
 
     public Optional<Accident> findById(int id) {
@@ -89,5 +90,54 @@ public class AccidentJdbcStore implements Store<Accident> {
 
     public void removeAll() {
 
+    }
+
+    private final RowMapper<Accident> accidentRowMapper = (resultSet, rowNum) -> {
+        Accident accident = new Accident();
+        accident.setId(resultSet.getInt("accidents_id"));
+        accident.setName(resultSet.getString("accidents_name"));
+        accident.setText(resultSet.getString("accidents_description"));
+        accident.setAddress(resultSet.getString("accidents_address"));
+
+        Type type = new Type();
+        type.setId(resultSet.getInt("types_id"));
+        type.setName(resultSet.getString("types_name"));
+        accident.setType(type);
+        Set<Rule> rules = getRules(resultSet.getInt("accidents_id"));
+        accident.setRules(rules);
+        return accident;
+    };
+
+    private final RowMapper<Rule> ruleRowMapper = (resultSet, rowNum) -> {
+        Rule rule = new Rule();
+        rule.setId(resultSet.getInt("rule_id"));
+        rule.setName(resultSet.getString("rules_name"));
+        return rule;
+    };
+
+/*    private final ResultSetExtractor<Set<Rule>> setResultSetExtractor = (rs -> {
+       rs.
+    });*/
+
+    public Set<Rule> getRules(int id) {
+        String sql = """
+                    SELECT
+                    rules.id rule_id,
+                    rules.name rules_name
+                    FROM accidents_rules
+                    INNER JOIN rules
+                    ON accidents_rules.rule_id = rules.id
+                    WHERE accidents_rules.accident_id = ?;
+                    """;
+/*        Set<Rule> rules = new HashSet<>();*/
+/*        Rule ruleFromResultSet = jdbc.queryFo(sql, (resultSet, rowNum) -> {
+            Rule rule = new Rule();
+            rule.setId(resultSet.getInt("rule_id"));
+            rule.setName(resultSet.getString("rules_name"));
+            return rule;
+        }, id);*/
+        List<Rule> ruleList = jdbc.query(sql, ruleRowMapper, id);
+/*        rules.add(ruleFromResultSet);*/
+        return new HashSet<>(ruleList);
     }
 }
