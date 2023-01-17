06926d728c9b2515c30b8507f6baa7e59360d326
ftptpf <inboxru@yandex.ru>
2023-01-17 09:48:20 +0300

    removeAll and remove in AccidentJdbcStore

--- src/main/java/ru/job4j/accidents/repository/jdbc/AccidentJdbcStore.java
+++ src/main/java/ru/job4j/accidents/repository/jdbc/AccidentJdbcStore.java
@@ -3,9 +3,7 @@ package ru.job4j.accidents.repository.jdbc;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.RowMapperResultSetExtractor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.Rule;
@@ -19,78 +17,6 @@ import java.util.*;
 @AllArgsConstructor
 public class AccidentJdbcStore implements Store<Accident> {
     private final JdbcTemplate jdbc;
-
-    public Optional<Accident> create(Accident accident) {
-        return Optional.empty();
-    }
-
-    public Collection<Accident> findAll() {
-        /*return jdbc.query(
-                """
-                    SELECT
-                    accidents.id accidents_id,
-                    accidents.name accidents_name,
-                    accidents.description accidents_description,
-                    accidents.address accidents_address,
-                    types.id types_id,
-                    types.name types_name,
-                    rules.name rules_name
-                    FROM accidents
-                    INNER JOIN types
-                    ON accidents.type_id = types.id
-                    INNER JOIN accidents_rules
-                    ON accidents.id = accidents_rules.accident_id
-                    INNER JOIN rules
-                    ON accidents_rules.rule_id = rules.id;
-                    """,
-                (resultSet, rowNum) -> {
-                    Accident accident = new Accident();
-                    Type type = new Type();
-                    type.setId(resultSet.getInt("types_id"));
-                    type.setName(resultSet.getString("types_name"));
-                    accident.setId(resultSet.getInt("accidents_id"));
-                    accident.setName(resultSet.getString("accidents_name"));
-                    accident.setText(resultSet.getString("accidents_description"));
-                    accident.setAddress(resultSet.getString("accidents_address"));
-                    accident.setType(type);
-                    accident.setRules(new HashSet<>());
-                    return accident;
-                });*/
-        String sql = """
-                SELECT
-                accidents.id accidents_id,
-                accidents.name accidents_name,
-                accidents.description accidents_description,
-                accidents.address accidents_address,
-                types.id types_id,
-                types.name types_name,
-                rules.name rules_name
-                FROM accidents
-                INNER JOIN types
-                ON accidents.type_id = types.id
-                INNER JOIN accidents_rules
-                ON accidents.id = accidents_rules.accident_id
-                INNER JOIN rules
-                ON accidents_rules.rule_id = rules.id;
-                """;
-        return jdbc.query(sql, accidentRowMapper);
-    }
-
-    public Optional<Accident> findById(int id) {
-        return Optional.empty();
-    }
-
-    public boolean update(Accident accident) {
-        return false;
-    }
-
-    public boolean remove(int id) {
-        return false;
-    }
-
-    public void removeAll() {
-
-    }
 
     private final RowMapper<Accident> accidentRowMapper = (resultSet, rowNum) -> {
         Accident accident = new Accident();
@@ -115,11 +41,75 @@ public class AccidentJdbcStore implements Store<Accident> {
         return rule;
     };
 
-/*    private final ResultSetExtractor<Set<Rule>> setResultSetExtractor = (rs -> {
-       rs.
-    });*/
+    public Optional<Accident> create(Accident accident) {
+        return Optional.empty();
+    }
 
-    public Set<Rule> getRules(int id) {
+    public Collection<Accident> findAll() {
+        String sql = """
+                SELECT
+                accidents.id accidents_id,
+                accidents.name accidents_name,
+                accidents.description accidents_description,
+                accidents.address accidents_address,
+                types.id types_id,
+                types.name types_name
+                FROM accidents
+                INNER JOIN types
+                ON accidents.type_id = types.id;
+                """;
+        return jdbc.query(sql, accidentRowMapper);
+    }
+
+    public Optional<Accident> findById(int id) {
+        String sql = """
+                SELECT
+                accidents.id accidents_id,
+                accidents.name accidents_name,
+                accidents.description accidents_description,
+                accidents.address accidents_address,
+                types.id types_id,
+                types.name types_name
+                FROM accidents
+                INNER JOIN types
+                ON accidents.type_id = types.id
+                WHERE accidents.id = ?;
+                """;
+        Accident accident = jdbc.queryForObject(sql, accidentRowMapper, id);
+        return Optional.ofNullable(accident);
+    }
+
+    public boolean update(Accident accident) {
+        String sql = """
+                INSERT INTO
+                accidents ()
+                """;
+        return false;
+    }
+
+    public boolean remove(int id) {
+        String sqlDeleteAccidentsRules = """
+                DELETE FROM ONLY accidents_rules ar
+                WHERE ar.accident_id = ?;
+                """;
+        String sqlDeleteAccidents = """
+                DELETE FROM ONLY accidents a
+                WHERE a.id = ?;
+                """;
+        int deleteAccidentRule = jdbc.update(sqlDeleteAccidentsRules);
+        int d2 = jdbc.update(sqlDeleteAccidents);
+        return (deleteAccidentRule > 0 && d2 > 0);
+    }
+
+    public void removeAll() {
+        String sql = """
+                TRUNCATE accidents_rules, accidents RESTART IDENTITY;
+                """;
+        jdbc.update(sql);
+
+    }
+
+    private Set<Rule> getRules(int id) {
         String sql = """
                     SELECT
                     rules.id rule_id,
@@ -129,15 +119,7 @@ public class AccidentJdbcStore implements Store<Accident> {
                     ON accidents_rules.rule_id = rules.id
                     WHERE accidents_rules.accident_id = ?;
                     """;
-/*        Set<Rule> rules = new HashSet<>();*/
-/*        Rule ruleFromResultSet = jdbc.queryFo(sql, (resultSet, rowNum) -> {
-            Rule rule = new Rule();
-            rule.setId(resultSet.getInt("rule_id"));
-            rule.setName(resultSet.getString("rules_name"));
-            return rule;
-        }, id);*/
         List<Rule> ruleList = jdbc.query(sql, ruleRowMapper, id);
-/*        rules.add(ruleFromResultSet);*/
         return new HashSet<>(ruleList);
     }
 }
