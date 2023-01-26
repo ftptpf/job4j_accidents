c8a231201ae66c12fbcb76a4adbb239192b52e09
ftptpf <inboxru@yandex.ru>
2023-01-26 09:21:45 +0300

    AccidentHibernateStore begin and commit transaction

--- src/main/java/ru/job4j/accidents/repository/hbm/AccidentHibernateStore.java
+++ src/main/java/ru/job4j/accidents/repository/hbm/AccidentHibernateStore.java
@@ -9,6 +9,7 @@ import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.repository.Store;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Primary
@@ -19,49 +20,63 @@ public class AccidentHibernateStore implements Store<Accident> {
 
     public Optional<Accident> create(Accident accident) {
         try (Session session = sf.openSession()) {
-/*            int id = (int) session.save(accident);
-            return id != 0 ? Optional.of(accident) : Optional.empty();*/
-            session.saveOrUpdate(accident);
-            return Optional.of(accident);
+            session.beginTransaction();
+            int id = (int) session.save(accident);
+            session.getTransaction().commit();
+            return id != 0 ? Optional.of(accident) : Optional.empty();
         }
     }
 
     public Collection<Accident> findAll() {
         try (Session session = sf.openSession()) {
-            return session.createQuery("FROM Accident a JOIN FETCH a.rules", Accident.class)
+            session.beginTransaction();
+            List<Accident> accidentList = session.createQuery(
+                    "FROM Accident a JOIN FETCH a.rules ORDER BY a.id", Accident.class)
                     .getResultStream()
                     .distinct()
                     .toList();
+            session.getTransaction().commit();
+            return accidentList;
         }
     }
 
     public Optional<Accident> findById(int id) {
         try (Session session = sf.openSession()) {
-            return session.createQuery("FROM Accident a JOIN FETCH a.rules WHERE a.id = :fId", Accident.class)
+            session.beginTransaction();
+            Optional<Accident> accidentOptional = session.createQuery(
+                    "FROM Accident a JOIN FETCH a.rules WHERE a.id = :fId", Accident.class)
                     .setParameter("fId", id)
                     .uniqueResultOptional();
+            session.getTransaction().commit();
+            return accidentOptional;
         }
     }
 
     public boolean update(Accident accident) {
         try (Session session = sf.openSession()) {
+            session.beginTransaction();
             session.update(accident);
+            session.getTransaction().commit();
             return true;
         }
     }
 
     public boolean remove(int id) {
         try (Session session = sf.openSession()) {
+            session.beginTransaction();
             Accident accident = (Accident) session.createQuery("DELETE FROM Accident a WHERE a.id = :fId")
                     .setParameter("fId", id)
                     .getSingleResult();
+            session.getTransaction().commit();
             return accident.getId() == id;
         }
     }
 
     public void removeAll() {
         try (Session session = sf.openSession()) {
+            session.beginTransaction();
             session.createSQLQuery("TRUNCATE accidents_rules, accidents RESTART IDENTITY");
+            session.getTransaction().commit();
         }
     }
 }
